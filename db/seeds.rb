73faee515b230337e4eb55fe64ab2a646aba319f
Rodrigo Ruas <rodrigoruas2@gmail.com>
2017-02-14 14:03:39 -0200

    feito

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "Travel", description: "Go to the airport next friday", status: false)
+
+Task.create(name: "Workshop", description: "Le Wagon workshop", status: false)
+
+Task.create(name: "Study", description: "Study a lot of Rails!!", status: false)
+
